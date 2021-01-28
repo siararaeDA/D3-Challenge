620423bfc83735fe4d5af27095e936417a294a51
Siara Leininger <leinings@oregonstate.edu>
2021-01-27 23:52:16 -0800

    Adding y axis label.

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -74,7 +74,15 @@ d3.csv("assets/data/data.csv").then(function(healthData) {
     chartGroup.append("text")
         .attr("transform", `translate(${chartWidth / 2}, ${chartHeight + margin.bottom - 15})`)
         .style("text-anchor", "middle")
-        .text("Poverty")
+        .text("In Poverty (%)")
+
+    chartGroup.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - margin.left)
+        .attr("x", 0 - (chartHeight / 2))
+        .attr("dy", "1em")
+        .style("text-anchor", "middle")
+        .text("Obesity (%)")
 
 
 } , function(error) {
